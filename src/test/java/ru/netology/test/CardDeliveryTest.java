22d8f67fe16290f4dcbbf63e09e05b4aaa5209ad
KiraKoddy <flatcher2007@narod.ru>
2022-09-26 17:56:27 +0300

    initial commit

---
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -0,0 +1,53 @@
+package ru.netology.test;
+
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataGenerator;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+class CardDeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(firstMeetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldBe(visible).should(exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(secondMeetingDate);
+        $(".button").click();
+        $$(".button").find(exactText("Перепланировать")).click();
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldBe(visible).should(exactText("Встреча успешно запланирована на " + secondMeetingDate));
+    }
+}
